68673981e20fc84e1aa57c56d9cd53c561d306ad
Sergey Rogozhin <sergeyrogojin@gmail.com>
2022-10-11 09:54:41 +0700

    init commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,30 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-import './Header.css'
+import "./Header.css";
 
-export const Header = ({addTodo}) => {
-  const [label, setLabel] = useState('')
+export const Header = ({ addTodo }) => {
+  const [label, setLabel] = useState("");
 
-  const handleChange = (event) => setLabel(event.target.value)
+  const handleChange = (event) => setLabel(event.target.value);
 
   const handleSubmit = (event) => {
-    event.preventDefault()
-    addTodo(label)
-    setLabel('')
-  }
+    event.preventDefault();
+    addTodo(label);
+    setLabel("");
+  };
 
   return (
-      <header className="header">
-        <h1>todos</h1>
-        <form onSubmit={handleSubmit}>
-          <input className="new-todo" placeholder="What needs to be done?" autoFocus={true} onChange={handleChange}
-                 value={label}/>
-        </form>
-      </header>
-  )
-}
+    <header className="header">
+      <h1>TODOS</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="new-todo"
+          placeholder="What needs to be done?"
+          autoFocus={true}
+          onChange={handleChange}
+          value={label}
+        />
+      </form>
+    </header>
+  );
+};
